3c2992c059edbaf058d795a9f7bc2c7a7e415792
Idoia Rojo Lazaro <idoia@ifeelonline.com>
2022-06-03 13:56:30 +0100

    root page

--- app/root.tsx
+++ app/root.tsx
@@ -1,9 +1,5 @@
-import type {
-  LinksFunction,
-  LoaderFunction,
-  MetaFunction,
-} from "@remix-run/node";
-import { json } from "@remix-run/node";
+import * as React from "react";
+import type { LoaderFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -11,44 +7,186 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useCatch,
+  useLoaderData,
+  useLocation,
 } from "@remix-run/react";
 
-import tailwindStylesheetUrl from "./styles/tailwind.css";
-import { getUser } from "./session.server";
+import {
+  ScriptToCheckThemeAlreadyApplied,
+  ThemeProvider,
+  useTheme,
+} from "~/utils/theme-provider";
+import type { Theme } from "~/utils/theme-provider";
+
+import clsx from "clsx";
+import type { LinksFunction } from "@remix-run/react/routeModules";
+import { getThemeSession } from "~/utils/theme.server";
+
+import styles from "./styles.css";
 
 export const links: LinksFunction = () => {
-  return [{ rel: "stylesheet", href: tailwindStylesheetUrl }];
+  return [{ rel: "stylesheet", href: styles }];
 };
 
-export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "Remix Notes",
-  viewport: "width=device-width,initial-scale=1",
-});
-
-type LoaderData = {
-  user: Awaited<ReturnType<typeof getUser>>;
+export type LoaderData = {
+  theme: Theme | null;
 };
+
 
 export const loader: LoaderFunction = async ({ request }) => {
-  return json<LoaderData>({
-    user: await getUser(request),
-  });
+  const themeSession = await getThemeSession(request);
+
+  const data: LoaderData = {
+    theme: themeSession.getTheme(),
+  };
+
+  return data;
 };
 
 export default function App() {
+  const data = useLoaderData<LoaderData>();
+
   return (
-    <html lang="en" className="h-full">
-      <head>
-        <Meta />
-        <Links />
-      </head>
-      <body className="h-full">
+    <ThemeProvider specifiedTheme={data.theme}>
+      <Document>
         <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
+      </Document>
+    </ThemeProvider>
   );
 }
+
+function Document({
+  children,
+  title,
+}: {
+  children: React.ReactNode;
+  title?: string;
+}) {
+  const [theme] = useTheme();
+  const data = useLoaderData<LoaderData>();
+
+  return (
+    <React.StrictMode>
+      <html lang="en" className={clsx(theme)}>
+        <head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width,initial-scale=1" />
+          {title ? <title>{title}</title> : null}
+          <Meta />
+          <Links />
+          <ScriptToCheckThemeAlreadyApplied ssrTheme={Boolean(data.theme)} />
+        </head>
+        <body className="font-inter">
+          {children}
+          <RouteChangeAnnouncement />
+          <ScrollRestoration />
+          <Scripts />
+          {process.env.NODE_ENV === "development" && <LiveReload />}
+        </body>
+      </html>
+    </React.StrictMode>
+  );
+}
+
+
+export function CatchBoundary() {
+  const caught = useCatch();
+
+  let message;
+  switch (caught.status) {
+    case 401:
+      message = (
+        <p>
+          Oops! Looks like you tried to visit a page that you do not have access
+          to.
+        </p>
+      );
+      break;
+    case 404:
+      message = (
+        <p>Oops! Looks like you tried to visit a page that does not exist.</p>
+      );
+      break;
+
+    default:
+      throw new Error(caught.data || caught.statusText);
+  }
+
+  return (
+    <Document title={`${caught.status} ${caught.statusText}`}>
+      <h1>
+        {caught.status}: {caught.statusText}
+      </h1>
+      {message}
+    </Document>
+  );
+}
+
+export function ErrorBoundary({ error }: { error: Error }) {
+  console.error(error);
+
+  return (
+    <Document title="Error!">
+      <div>
+        <h1>Oh god everything is on fire please help.</h1>
+
+        <h2>A massive error exploded our servers. We are trying to fix it!</h2>
+      </div>
+    </Document>
+  );
+}
+
+const RouteChangeAnnouncement = React.memo(() => {
+  const [hydrated, setHydrated] = React.useState(false);
+  const [innerHtml, setInnerHtml] = React.useState("");
+  const location = useLocation();
+
+  React.useEffect(() => {
+    setHydrated(true);
+  }, []);
+
+  const firstRenderRef = React.useRef(true);
+  React.useEffect(() => {
+    // Skip the first render because we don't want an announcement on the
+    // initial page load.
+    if (firstRenderRef.current) {
+      firstRenderRef.current = false;
+      return;
+    }
+
+    const pageTitle = location.pathname === "/" ? "Home page" : document.title;
+    setInnerHtml(`Navigated to ${pageTitle}`);
+  }, [location.pathname]);
+
+  // Render nothing on the server. The live region provides no value unless
+  // scripts are loaded and the browser takes over normal routing.
+  if (!hydrated) {
+    return null;
+  }
+
+  return (
+    <div
+      aria-live="assertive"
+      aria-atomic
+      id="route-change-region"
+      style={{
+        border: "0",
+        clipPath: "inset(100%)",
+        clip: "rect(0 0 0 0)",
+        height: "1px",
+        margin: "-1px",
+        overflow: "hidden",
+        padding: "0",
+        position: "absolute",
+        width: "1px",
+        whiteSpace: "nowrap",
+        wordWrap: "normal",
+      }}
+    >
+      {innerHtml}
+    </div>
+  );
+});
+
+RouteChangeAnnouncement.displayName = 'RouteChangeAnnouncement';
