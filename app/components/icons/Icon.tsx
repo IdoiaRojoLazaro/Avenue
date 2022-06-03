7968f83faf7bb34e76447ec54f0d72abae748e0a
Idoia Rojo Lazaro <idoia@ifeelonline.com>
2022-06-03 14:16:31 +0100

    add toggle to change theme

--- app/components/icons/Icon.tsx
+++ app/components/icons/Icon.tsx
@@ -1,18 +1,27 @@
 import { useState } from 'react'
+import { Theme, useTheme } from '~/utils/theme-provider';
 import { MoonIconSvg } from './MoonIconSvg';
 import { SunIconSvg } from './SunIconSvg';
 
 export const Icon = () => {
+  const [theme, setTheme] = useTheme();
   const [animationClass, setAnimationClass] = useState<string>('');
 
   const handleClick = () => {
-    setAnimationClass('dark');
+    setAnimationClass('');
+    setTimeout(() => setAnimationClass('offset'), 100);
+
+    toggleTheme();
   }
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT));
+  };
 
   return (
 
     <button className={`icon ${animationClass}`} onClick={handleClick}>
-      {animationClass === '' ?
+      {theme === Theme.LIGHT ?
         MoonIconSvg
         :
         SunIconSvg
@@ -20,6 +29,3 @@ export const Icon = () => {
     </button >
   )
 }
-
-
-
